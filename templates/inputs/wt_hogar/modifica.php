3db0bdbf3c4b306ea30d1e4210f1da4476cf8696
ivan <ivan_avelar6@hotmail.com>
2022-07-21 17:30:59 -0500

    v0.5.2 integracion de update con los campos correspondientes

--- templates/inputs/wt_hogar/modifica.php
+++ templates/inputs/wt_hogar/modifica.php
@@ -1,36 +1,39 @@
+<?php /** @var base\controller\controlador_base $controlador  viene de registros del controler/lista */ ?>
+<?php use config\views; ?>
+<div class="control-group col-sm-6">
+    <label class="control-label" for="descripcion">Id</label>
+    <div class="controls">
+        <input type="text" name="descripcion" value="<?php echo $controlador->row_upd->id ?>" class="form-control" required="" id="Id" placeholder="Descripcion" readonly>
+    </div>
+</div>
 <div class="control-group col-sm-6">
     <label class="control-label" for="descripcion">Descripcion</label>
     <div class="controls">
-        <input type="text" name="descripcion" value="" class="form-control" required="" id="descripcion" placeholder="Descripcion">
+        <input type="text" name="descripcion" value="<?php echo $controlador->row_upd->descripcion ?>" class="form-control" required="" id="descripcion" placeholder="Descripcion">
     </div>
 </div>
 <div class="control-group col-sm-6">
     <label class="control-label" for="codigo">Codigo</label>
     <div class="controls">
-        <input type="text" name="codigo" value="" class="form-control" required="" id="codigo" placeholder="Codigo">
+        <input type="text" name="codigo" value="<?php echo $controlador->row_upd->codigo ?>" class="form-control" required="" id="codigo" placeholder="Codigo">
     </div>
 </div>
 <div class="control-group col-sm-6">
     <label class="control-label" for="observaciones">Observaciones</label>
     <div class="controls">
-        <input type="text" name="observaciones" value="" class="form-control" required="" id="observaciones" placeholder="Observaciones">
+        <input type="text" name="observaciones" value="<?php echo $controlador->row_upd->observaciones ?>" class="form-control" required="" id="observaciones" placeholder="Observaciones">
     </div>
 </div>
 <div class="control-group col-sm-6">
     <label class="control-label" for="url">Url</label>
     <div class="controls">
-        <input type="text" name="url" value="" class="form-control" required="" id="Url" placeholder="Url">
+        <input type="text" name="url" value="<?php echo $controlador->row_upd->url ?>" class="form-control" required="" id="Url" placeholder="Url">
     </div>
 </div>
 <div class="control-group col-sm-6">
     <label class="control-label" for="img_descripcion">Img Descripcion</label>
     <div class="controls">
-        <input type="text" name="img_descripcion" value="" class="form-control" required="" id="Url" placeholder="img_descripcion">
+        <input type="text" name="img_descripcion" value="<?php echo $controlador->row_upd->descripcion ?>" class="form-control" required="" id="Url" placeholder="img_descripcion">
     </div>
 </div>
-<div class="control-group btn-alta">
-    <div class="controls">
-        <button type="submit" class="btn btn-success" name="guarda">Alta</button>
-        <button type="submit" class="btn btn-success" name="guarda_otro">Genero Otro</button>
-    </div>
-</div>
+<?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>

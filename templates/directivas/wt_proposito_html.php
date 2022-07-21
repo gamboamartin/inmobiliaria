610a4ede33223591ab74061e405923d005d140ee
ivan <ivan_avelar6@hotmail.com>
2022-07-21 18:25:35 -0500

    v0.6.2 se Inicializa CRUD wt_proposito

---
+++ templates/directivas/wt_proposito_html.php
@@ -0,0 +1,12 @@
+<?php
+namespace html;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\html_controler;
+use PDO;
+use stdClass;
+
+class wt_proposito_html extends html_controler {
+
+
+}

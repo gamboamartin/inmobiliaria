1e0d7dd6f304cd324b9453a597510ec9b688a3cb
ivan <ivan_avelar6@hotmail.com>
2022-07-22 17:47:02 -0500

    v0.8.2 Se ajustan los campos correspondientes en alta, modifica y lista de wt_proposito

---
+++ templates/listas/wt_proposito/row.php
@@ -0,0 +1,13 @@
+<?php use config\views; ?>
+<?php /** @var stdClass $row  viene de registros del controler*/ ?>
+
+<tr>
+    <td><?php echo $row->wt_proposito_id; ?></td>
+    <td><?php echo $row->wt_proposito_descripcion; ?></td>
+    <td><?php echo $row->wt_proposito_codigo; ?></td>
+    <!-- Dynamic generated -->
+
+    <!-- End dynamic generated -->
+
+    <?php include (new views())->ruta_templates.'listas/action_row.php';?>
+</tr>

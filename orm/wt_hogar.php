5d0a81ca686e463e1b0edb46ed0c2510dd269054
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-07-26 17:39:11 -0500

    v0.12.2 Se agrega funcion obtener_registro_wt_hogar a modelo wt_hogar

--- orm/wt_hogar.php
+++ orm/wt_hogar.php
@@ -18,4 +18,33 @@ class wt_hogar extends modelo{
             columnas: $columnas,no_duplicados: $no_duplicados);
     }
 
+    public function obtener_registro_wt_hogar(string $landing_url): array{
+        $wt_hogar_url_buscado['url'] = $landing_url;
+
+        $r_wt_hogar = $this->filtro_and(filtro: $wt_hogar_url_buscado, tipo_filtro: 'textos');
+        if(errores::$error){
+            $error = (new errores())->error(mensaje: 'Error al obtener registro hogar con url busca',data:  $r_wt_hogar);
+            print_r($error);
+            die('Error');
+        }
+
+        $wt_hogar = array();
+        if(isset($r_wt_hogar->registros_obj) && count($r_wt_hogar->registros_obj) > 0){
+            $wt_hogar_id = $r_wt_hogar->registros_obj[0]->wt_hogar_id;
+            if(errores::$error){
+                $error = (new errores())->error(mensaje: 'Error al obtener id hogar',data:  $wt_hogar_id);
+                print_r($error);
+                die('Error');
+            }
+
+            $wt_hogar = $this->registro(registro_id: $wt_hogar_id);
+            if(errores::$error){
+                $error = (new errores())->error(mensaje: 'Error al obtener hogar',data:  $wt_hogar);
+                print_r($error);
+                die('Error');
+            }
+        }
+        return $wt_hogar;
+    }
+
 }

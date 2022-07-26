b045043adde0191f20b7598f5ee3d79f5249d1fd
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-07-26 17:41:55 -0500

    v0.12.3 FIX al modificar img_descripcion

--- templates/inputs/wt_hogar/modifica.php
+++ templates/inputs/wt_hogar/modifica.php
@@ -7,43 +7,43 @@ use config\views;
 <div class="control-group col-sm-6">
     <label class="control-label" for="descripcion">Id</label>
     <div class="controls">
-        <input type="text" name="descripcion" value="<?php echo $controlador->row_upd->id ?>" class="form-control" required="" id="Id" placeholder="Descripcion" readonly>
+        <input type="text" name="descripcion" value="<?php echo $controlador->row_upd->id; ?>" class="form-control" required="" id="Id" placeholder="Descripcion" readonly>
     </div>
 </div>
 <div class="control-group col-sm-6">
     <label class="control-label" for="descripcion">Descripcion</label>
     <div class="controls">
-        <input type="text" name="descripcion" value="<?php echo $controlador->row_upd->descripcion ?>" class="form-control" required="" id="descripcion" placeholder="Descripcion">
+        <input type="text" name="descripcion" value="<?php echo $controlador->row_upd->descripcion; ?>" class="form-control" required="" id="descripcion" placeholder="Descripcion">
     </div>
 </div>
 <div class="control-group col-sm-6">
     <label class="control-label" for="codigo">Codigo</label>
     <div class="controls">
-        <input type="text" name="codigo" value="<?php echo $controlador->row_upd->codigo ?>" class="form-control" required="" id="codigo" placeholder="Codigo">
+        <input type="text" name="codigo" value="<?php echo $controlador->row_upd->codigo; ?>" class="form-control" required="" id="codigo" placeholder="Codigo">
     </div>
 </div>
 <div class="control-group col-sm-6">
     <label class="control-label" for="observaciones">Observaciones</label>
     <div class="controls">
-        <input type="text" name="observaciones" value="<?php echo $controlador->row_upd->observaciones ?>" class="form-control" required="" id="observaciones" placeholder="Observaciones">
+        <input type="text" name="observaciones" value="<?php echo $controlador->row_upd->observaciones; ?>" class="form-control" required="" id="observaciones" placeholder="Observaciones">
     </div>
 </div>
 <div class="control-group col-sm-6">
     <label class="control-label" for="url">Url</label>
     <div class="controls">
-        <input type="text" name="url" value="<?php echo $controlador->row_upd->url ?>" class="form-control" required="" id="Url" placeholder="Url">
+        <input type="text" name="url" value="<?php echo $controlador->row_upd->url; ?>" class="form-control" required="" id="Url" placeholder="Url">
     </div>
 </div>
 <div class="control-group col-sm-6">
     <label class="control-label" for="img_descripcion">Img Descripcion</label>
     <div class="controls">
-        <input type="text" name="img_descripcion" value="<?php echo $controlador->row_upd->descripcion ?>" class="form-control" required="" id="Url" placeholder="img_descripcion">
+        <input type="text" name="img_descripcion" value="<?php echo $controlador->row_upd->img_descripcion; ?>" class="form-control" required="" id="Url" placeholder="img_descripcion">
     </div>
 </div>
 <div class="control-group col-sm-6">
     <label class="control-label" for="georeferencia">Georeferencia</label>
     <div class="controls">
-        <input type="text" name="georeferencia" value="<?php echo $controlador->row_upd->georeferencia ?>" class="form-control" required="" id="georeferencia" placeholder="Georeferencia">
+        <input type="text" name="georeferencia" value="<?php echo $controlador->row_upd->georeferencia; ?>" class="form-control" required="" id="georeferencia" placeholder="Georeferencia">
     </div>
 </div>
 <div class="control-group col-sm-6">

ffdf6cddb757f32dcc5ee22ac4082953bec67366
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-09-27 18:51:40 -0500

    v0.46.4 se genera crud wt_estado

---
+++ controllers/controlador_wt_estado.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace controllers;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\links_menu;
+use gamboamartin\system\system;
+use gamboamartin\template\html;
+use html\wt_estado_html;
+use html\wt_proposito_html;
+use models\wt_estado;
+use PDO;
+use stdClass;
+
+class controlador_wt_estado extends system {
+
+    public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
+        $modelo = new wt_estado(link: $link);
+        $html_base = new html();
+        $html = new wt_estado_html(html: $html_base);
+        $obj_link = new links_menu($this->registro_id);
+        parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
+
+        $this->titulo_lista = 'Estado';
+
+    }
+
+
+
+}
